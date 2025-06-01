e9ea4dbe956913824ee5a83df72c04c95bb7ee2b
Philip Saa <cowglow@gmail.com>
2025-06-01 10:01:31 +0200

    Updated text layout on ZPL command line

--- src/convert-txt-to-zpl.ts
+++ src/convert-txt-to-zpl.ts
@@ -1,8 +1,17 @@
-export function convertTxtToZpl(input:string) {
+export function convertTxtToZpl(input: string) {
     const marginX = 118;
     const eventCode = "#JSCC25";
-    const zpl = `^XA^PW1476^LL1205^FO${marginX},100^A0,N,136,136^FD${eventCode}^FS^FO${marginX},300^A0,N,100,100^FD${input}^FS^XZ`;
+    const zpl = `^XA
+^PW1795
+^LL1205
+^FO${marginX},100^A0,N,136,136^FD${eventCode}^FS
+^FO${marginX},300^A0,N,80,80
+^FB936,4,20,L,0
+^FD${input}^FS
+^XZ`;
     return zpl;
+    // Philip Saa // Saab //  // Indie Web // A/V // React // Svelte // TypeScript // Playwright
+
     // const label = new Label();
     // label.printDensity = new PrintDensity(PrintDensityName['12dpmm']);
     // label.width = 600;
