857f1094516a782947e72057bc111c9a1dba5977
Philip Saa <cowglow@gmail.com>
2025-06-03 21:21:37 +0200

    Add Window variation for ncat - Pre-Printer

--- server.ts
+++ server.ts
@@ -32,4 +32,8 @@ fastify.listen({port: 9100}).then(() => {
     console.log("==========================================================")
 });
 
+// Example command to send ZPL to a printer from Mac (nc):
 // echo "^XA^FO10,100^A0,N,136,136^FD#JSCC25^FS^FO100,240^A0,N,100,100^FDPHILIP SAA^FS^XZ" | nc 192.168.1.16 9100
+
+// Example command to send ZPL to a printer from Windows (ncat):
+// echo "^XA^FO10,100^A0,N,136,136^FD#JSCC25^FS^FO100,240^A0,N,100,100^FDPHILIP SAA^FS^XZ" | ncat 192.168.1.16 9100
